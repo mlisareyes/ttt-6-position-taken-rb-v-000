b7537bbce85ae61325700472d43603305221ba59
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-04-07 03:33:13 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken? (board, position)
-  if board[position] == " " || board [position] == " " || board[position] == nil
+  if board[position] == " " || board [position] == " " || board[position]
     return false
   elsif board[position] == "X" || board [position] == "O"
     return true
